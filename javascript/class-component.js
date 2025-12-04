0f8df3ae9e7cde391010f711e24b019237ecb615
Lope879 <100965607+Lope879@users.noreply.github.com>
2025-12-04 15:18:51 -0800

    feat: implemented js for local component creation

--- javascript/class-component.js
+++ javascript/class-component.js
@@ -8,21 +8,24 @@ class ClassCard extends HTMLElement {
         const code = this.getAttribute('code') !== "undefined" ? this.getAttribute('code') : 'Unknown code';
         const name = this.getAttribute('name') !== "undefined" ? this.getAttribute('name') : 'Unknown name';
         const note = this.getAttribute('note') !== "undefined" ? this.getAttribute('note') : 'Older picture. Nothing to say here!';
+        const link = this.getAttribute('link') !== "undefined" ? this.getAttribute('link') : '#';
         const imageUrl = this.getAttribute('image-url') || '';
 
         this.innerHTML = '';
         this.innerHTML += `
-            <div class="simple-card">
-                <hgroup>
-                <p class="field">${field} ${code}</p>
-                <h2 class="name">${name}</h2>
-                </hgroup>
-                <p class="note">${note}</p>
-                <img src="${imageUrl}" alt="${name} image"/>
-            </div>
+            <div class="class-card">
+                    <hgroup>
+                        <p>${field} ${code}</p>
+                        <h2>${name}</h2>
+                        <a href="${link}">Course Info</a>
+                    </hgroup>
+                    <picture>
+                        <img src="${imageUrl}" alt="${name} Class Image">
+                    </picture>
+                    <p class="class-note">${note}</p>
+                </div>
         `;
-        customElements.define('class-card', ClassCard);
     }
-
-    
 }
+
+customElements.define('class-card', ClassCard);
